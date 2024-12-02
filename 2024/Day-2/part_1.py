24fda0c0171946ec36a74f6aff955242ad820d1d
Shatansh <shatansh.iitkgp@gmail.com>
2024-12-02 13:02:08 +0530

    Add Day-2 Solutions in Python

--- 2024/Day-2/part_1.py
+++ 2024/Day-2/part_1.py
@@ -4,43 +4,20 @@ import re
 
 def parse_stuff(file_path):
     with open(file_path) as file:
-        lines = file.read().split("\n")
-
-    reports = []
-
-    for line in lines:
-        matches = re.findall(r"\S+", line)
-        each_entry = []
-        for match in matches:
-            each_entry.append(int(match))
-        reports.append(each_entry)
-
-    return reports
+        return [[int(match) for match in re.findall(r"\S+", line)] for line in file.read().split("\n")]
 
 
-def check_inc_seq(arr):
-    num = len(arr)
-
-    for i in range(1, num):
-        if arr[i] <= arr[i - 1]:
+def check_seq(arr, flag):
+    for i in range(1, len(arr)):
+        if flag and arr[i] <= arr[i - 1]:
             return 0
-    return 1
-
-
-def check_dec_seq(arr):
-    num = len(arr)
-
-    for i in range(1, num):
-        if arr[i] >= arr[i - 1]:
+        elif flag == 0 and arr[i] >= arr[i-1]:
             return 0
-
     return 1
 
 
 def check_max_diff(arr):
-    num = len(arr)
-
-    for i in range(1, num):
+    for i in range(1, len(arr)):
         if abs(arr[i] - arr[i - 1]) < 1 or abs(arr[i] - arr[i - 1]) > 3:
             return 0
 
@@ -48,19 +25,13 @@ def check_max_diff(arr):
 
 
 if __name__ == "__main__":
-    _file_path = os.path.join(os.path.abspath(os.getcwd()), "data.txt")
+    file_path = os.path.join(os.path.abspath(os.getcwd()), "data.txt")
 
-    reports = parse_stuff(_file_path)
+    reports = parse_stuff(file_path)
     safe_reports = 0
 
     for report in reports:
         if report:
-            flag = 0
-
-            flag = (check_inc_seq(report) or check_dec_seq(report)) and check_max_diff(
-                report
-            )
-
-            safe_reports += flag
+            safe_reports += (check_seq(report, 0) or check_seq(report, 1)) and check_max_diff(report)
 
     print(safe_reports)
--- 2024/Day-2/part_2.py
+++ 2024/Day-2/part_2.py
@@ -1,27 +1,14 @@
+# Things to learn: List Comprehension and all method
+
 import os
 import re
 
-
 def parse_the_file(file_path):
     with open(file_path) as file:
-        lines = file.read().split("\n")
-
-    reports = []
-
-    for line in lines:
-        matches = re.findall(r"\S+", line)
-        each_entry = []
-        for match in matches:
-            each_entry.append(int(match))
-        reports.append(each_entry)
-
-    return reports
-
+        return [[int(match) for match in re.findall(r"\S+", line)] for line in file.read().split("\n")]
 
 def check_seq(arr, flag):
-    num = len(arr)
-
-    for i in range(1, num):
+    for i in range(1, len(arr)):
         if flag and arr[i] <= arr[i - 1]:
             return 0
         elif flag == 0 and arr[i] >= arr[i-1]:
@@ -30,18 +17,11 @@ def check_seq(arr, flag):
 
 
 def check_max_diff(arr):
-    num = len(arr)
-
-    for i in range(1, num):
+    for i in range(1, len(arr)):
         if abs(arr[i] - arr[i - 1]) < 1 or abs(arr[i] - arr[i - 1]) > 3:
             return 0
 
     return 1
-
-def delete_element(arr, i):
-    arr = arr[:]
-    arr.pop(i)
-    return arr
 
 def check(arr):
     return bool(arr and (check_seq(arr, 0) or check_seq(arr, 1)) and check_max_diff(arr)) 
@@ -54,20 +34,13 @@ if __name__ == "__main__":
     safe_reports = 0
     
     for report in reports:
-        ultimate_flag = 0
         ultimate_flag = check(report)
-        safe_reports+=(1 and ultimate_flag)
+        safe_reports += (1 and ultimate_flag)
         
         if not ultimate_flag:
-            num = len(report)
-            for i in range(num):
-                updated_report = delete_element(report, i)
-                ultimate_flag = check(updated_report)
-                
-                if ultimate_flag:
+            for i in range(len(report)):
+                if check(report[0:i] + report[i+1:]):
                     safe_reports+=1
                     break
-
-
 
     print(safe_reports)
